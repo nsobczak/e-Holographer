2d6c5d41f7b2534537ec7dadf3997c4bb40c27fc
Vincent Reynaert <v.vincent59230@laposte.net>
2016-06-25 13:10:51 +0200

    Corrections
    Corrections de :
        - l'indentation sur github (car elle a été altérée)
        - la fonction imagesPlateauInitial (la liste des positions des pièces été erronée)
        - la fonction creeImageHolographique est renommée regroupe4ImagesEn1 et un "+1" intempestif a été retiré

--- Echec 3D - hologramme/FusionImagesCreationPlateau.py
+++ Echec 3D - hologramme/FusionImagesCreationPlateau.py
@@ -35,11 +35,11 @@ def imageEnTableau( listeImages ):
         #Lecture des images et ajout dans la liste  
         imageLue = plt.imread(image)            # lit l'image /!\ uniquement PNG /!\
         #Conversion des images sous forme de tableau  
-        imageTab = imageLue[:, :, 0:3]     # syntaxe sous matplotlib pour transformer l'image en tableau de pixels RVB
+        imageTab = imageLue[:, :, 0:3]     	# syntaxe sous matplotlib pour transformer l'image en tableau de pixels RVB (on enlève le canal alpha)
         #Ajout à la liste finale
         imageTableau += [ imageTab ]
         #Fermeture de l'image
-		plt.close(image)
+	plt.close(image)
 
         
     return imageTableau
@@ -50,33 +50,31 @@ def imageEnTableau( listeImages ):
 #%%____________________________________________________________________________
 # Fonctions de traitement sur deux images (25lignes)
 def superpose2Images(base, img, imgF):
-	"""fonction qui crée une image à partir de la superposition de 2 images en comparaison à 1 base (plateau vide)
-			paramètres : - 1 tableau = l'image de la base utilisée pour réaliser la comparaison
-						 - 1 tableau = l'image qui sera superposée à l'image que l'on veut modifier
-						 - 1 tableau = l'image que l'on veut modifier
-			retourne : 1 tableau = l'image modifiée
+    """fonction qui crée une image à partir de la superposition de 2 images en comparaison à 1 base (plateau vide)
+	paramètres : 	- 1 tableau = l'image de la base utilisée pour réaliser la comparaison
+			- 1 tableau = l'image qui sera superposée à l'image que l'on veut modifier
+			- 1 tableau = l'image que l'on veut modifier
+	retourne : 1 tableau = l'image modifiée
     """
    
     #Begin
-	print("Traitement en cours...")
-
-	#Calcul du nombre de pixels à fusionner
-	if ( (len(base[0]), len(base)) == (len(img[0]), len(img)) ) :
-		colonnes = len(base[0])
-		lignes = len(base)
-	else :
-		return ("Erreur : les deux images n'ont pas les même dimentions")
-
-	#Calcul de la valeur des pixels (ligne puis colonne)
-	for i in range(lignes):
-		for j in range(colonnes):
-			p1 = base[i][j]
-			p2 = img[i][j]
-			if ( (p1[0] != p2[0]) and (p1[1] != p2[1]) and (p1[2] != p2[2]) ) : #comparaison des RVB du pixel
-				imgF[i][j] = p2
-
-	return imgF
-	#End
+    print("Traitement en cours...")
+    
+    #Calcul du nombre de pixels à fusionner et vérification que les deux images sont de mêmes dimensions
+    if ( (len(base[0]), len(base)) == (len(img[0]), len(img)) ) :
+    	colonnes = len(base[0])
+    	lignes = len(base)
+    else :
+	return ("Erreur : les deux images n'ont pas les même dimensions")
+    #Calcul de la valeur des pixels (ligne puis colonne)
+    for i in range(lignes):
+    	for j in range(colonnes):
+            p1 = base[i][j]
+	    p2 = img[i][j]
+	    if ( (p1[0] != p2[0]) and (p1[1] != p2[1]) and (p1[2] != p2[2]) ) : #comparaison des niveaux de couleurs RVB des pixels
+		imgF[i][j] = p2
+    return imgF
+    #End
 
 
 
@@ -90,7 +88,7 @@ def superposePlusieursImages(listeImg, orientation):
             retourne : 1 tableau = l'image finale
     """
     #Begin
-    nbImg = len(listeImg)      # nombre d'images dans la liste
+    nbImg = len(listeImg)		 # nombre d'images dans la liste
     
     #Conversion des images en tableau
     imageTableau = imageEnTableau( listeImg )
@@ -108,8 +106,8 @@ def superposePlusieursImages(listeImg, orientation):
         return imgF
         
     else :
-		#Erreur : pas d'image dans la liste
-		return ("Erreur : il faut entrer une liste contenant des images")
+	#Erreur : pas d'image dans la liste
+	return ("Erreur : il faut entrer une liste contenant des images")
 
     #End
 
@@ -118,185 +116,181 @@ def superposePlusieursImages(listeImg, orientation):
 #%%____________________________________________________________________________
 # Fonctions de trie de la liste d'images a fusionner (37lignes)	
 def trieListeImageAFusionnerSelonOrientation(listeImageAFusionner, orientation):
-	""" fonction qui trie les images pour qu'elles soient dans le bon ordre : 
-		la pièce de devant 'ecrase' celle de derrière mais pas l'inverse 
-		pour respecter la perspective
-			paramètres : - 1 liste = contenant les images à superposer, mais ne 
-                            contenant pas l'image du plateau de jeu
-                         - 1 String = le point de vue pour la prise d'image du plateau
-			retourne : 1 liste = contenant les images à superposer, mais ne 
-                            contenant pas l'image du plateau de jeu
-	"""
-	#Begin
+    """ fonction qui trie les images pour qu'elles soient dans le bon ordre : 
+	la pièce de devant 'ecrase' celle de derrière mais pas l'inverse 
+	pour respecter la perspective
+	        paramètres : 	- 1 liste = contenant les images à superposer, mais ne 
+                            	  contenant pas l'image du plateau de jeu
+                         	- 1 String = le point de vue pour la prise d'image du plateau
+		retourne : 1 liste = contenant les images à superposer, mais ne 
+                           contenant pas l'image du plateau de jeu
+    """
+    #Begin
 
-	#On associe un indice à l'orientation pour permettre par la suite un traitement unique pour tous les cas
-	listeOrientations = ["back", "front", "right", "left"]
-	indiceOrientation = listeOrientations.index(orientation) 	#Trouve l'indice d'une orientation donnee dans la liste
+    #On associe un indice à l'orientation pour permettre par la suite un traitement unique pour tous les cas
+    listeOrientations = ["back", "front", "right", "left"]
+    indiceOrientation = listeOrientations.index(orientation) 	#Trouve l'indice d'une orientation donnee dans la liste
 	
-	#Cree une liste contenant les lettres A B C... H dans l'ordre
-	listeColonnes = []
-	for i in range(ord('A'),ord('H')+1):
-		listeColonnes += [chr(i)]			
-	
-	#On commence le trie	
-	print("on tourne pour ", orientation)
-	listeTriee = []
-	i = 1 + (7 * (indiceOrientation % 2)) # 8 quand indice est impaire {front et left} et 1 sinon {back et right}
-	print("soit un départ pour i = ", i)
-	compteur = 1
-	while (compteur < 9):
-		
-		k = 0
-		while (k < len(listeImageAFusionner)):
-			nomImage = listeImageAFusionner[k]
-			#On traite ensemble les cas {front et back}
-			if (indiceOrientation < 2):
-				if ((orientation in nomImage) and (str(i) in nomImage)):
-					listeTriee += [nomImage]
-					print("on modifie bien i et on rentre même dans le if indiceorientation qui va bien", i)
-			#On traite ensemble les cas {left et right}
-			else :
-				if ((orientation in nomImage) and (listeColonnes[i - 1] in nomImage)):
-					listeTriee += [nomImage]
-					print("on modifie bien i et on rentre même dans le if indiceorientation qui va bien", listeColonnes[i - 1])
-			k += 1
-		i += (-1)**indiceOrientation  # -1 quand indice est impaire {front et left} et +1 sinon {back et right}
-		compteur += 1
-		print("compteur", compteur -1, "et i = ", i)
+    #Cree une liste contenant les lettres A B C... H dans l'ordre
+    listeColonnes = []
+    for i in range(ord('A'),ord('H')+1):
+        listeColonnes += [chr(i)]			
 
-	return listeTriee
-	#End
+    #On commence le trie	
+    print("on tourne pour ", orientation)
+    listeTriee = []
+    i = 1 + (7 * (indiceOrientation % 2)) # 8 quand indice est impaire {front et left} et 1 sinon {back et right}
+    print("soit un départ pour i = ", i)
+    compteur = 1
+    while (compteur < 9):
+        k = 0
+	while (k < len(listeImageAFusionner)):
+            nomImage = listeImageAFusionner[k]
+            #On traite ensemble les cas {front et back}
+	    if (indiceOrientation < 2):
+		if ((orientation in nomImage) and (str(i) in nomImage)):
+	            listeTriee += [nomImage]
+		    print("on modifie bien i et on rentre même dans le if indiceorientation qui va bien", i)
+	    #On traite ensemble les cas {left et right}
+	    else :
+		if ((orientation in nomImage) and (listeColonnes[i - 1] in nomImage)):
+	            listeTriee += [nomImage]
+		    print("on modifie bien i et on rentre même dans le if indiceorientation qui va bien", listeColonnes[i - 1])
+	    k += 1
+	i += (-1)**indiceOrientation  # -1 quand indice est impaire {front et left} et +1 sinon {back et right}
+	compteur += 1
+	print("compteur", compteur -1, "et i = ", i)
+
+    return listeTriee
+    #End
 
 
 	
 #%%____________________________________________________________________________
 # Fonction qui produit les images initiales du plateau (19lignes)
 def imagesPlateauInitial(nomImage):
-	"""Fonction qui crée le plateau dans l'état de départ et
-		enregistre l'image fusionnee
-			paramètre : 1 String = le nom de l'image a enregistrer (sans l'orientation devant)
-			retourne : 1 liste = liste des images à fusionner (triee)
-	"""
-	#Begin
-
-	#On associe un indice à l'orientation pour permettre par la suite un traitement unique pour tous les cas
-	listeOrientations = ["back", "front", "right", "left"]
+    """Fonction qui crée le plateau dans l'état de départ et
+       enregistre l'image fusionnee
+                paramètre : 1 String = le nom de l'image a enregistrer (sans l'orientation devant)
+		retourne : 1 liste = liste des images à fusionner (triee)
+    """
+    #Begin
+    
+    #On associe un indice à l'orientation pour permettre par la suite un traitement unique pour tous les cas
+    listeOrientations = ["back", "front", "right", "left"]
 	
-	#Cree une liste contenant les lettres A B C... H dans l'ordre
-	listeColonnes = []
-	for i in range(ord('A'),ord('H')+1):
-		listeColonnes += [chr(i)]	
+    #Cree une liste contenant les lettres A B C... H dans l'ordre
+    listeColonnes = []
+    for i in range(ord('A'),ord('H')+1):
+	listeColonnes += [chr(i)]	
 	
-	#On commence a creer les images du plateau initial 
-	listeImages = ["WhiteRookA1", "WhiteKnightB1", "WhiteBishopC1", "WhiteQueenD1.png", "WhiteKingE1.png", "WhiteBishopF1", "WhiteKnightG1", "WhiteRookF1", "WhitePawnA2.png", "WhitePawnB2.png", "WhitePawnC2.png", "WhitePawnD2.png", "WhitePawnE2.png", "WhitePawnF2.png", "WhitePawnG2.png", "WhitePawnH2.png", "BlackRookA8", "BlackKnightB8", "BlackBishopC8", "BlackQueenD8.png", "BlackKingE8.png", "BlackBishopF8", "BlackKnightG8", "BlackRookF8", "BlackPawnA7.png", "BlackPawnB7.png", "BlackPawnC7.png", "BlackPawnD7.png", "BlackPawnE7.png", "BlackPawnF7.png", "BlackPawnG7.png", "BlackPawnH7.png"]
+    #On commence a creer les images du plateau initial 
+    listeImages = ["WhiteRookA1.png", "WhiteKnightB1.png", "WhiteBishopC1.png", "WhiteQueenD1.png", "WhiteKingE1.png", "WhiteBishopF1.png", "WhiteKnightG1.png", "WhiteRookH1.png", "WhitePawnA2.png", "WhitePawnB2.png", "WhitePawnC2.png", "WhitePawnD2.png", "WhitePawnE2.png", "WhitePawnF2.png", "WhitePawnG2.png", "WhitePawnH2.png", "BlackRookA8.png", "BlackKnightB8.png", "BlackBishopC8.png", "BlackQueenD8.png", "BlackKingE8.png", "BlackBishopF8.png", "BlackKnightG8.png", "BlackRookH8.png", "BlackPawnA7.png", "BlackPawnB7.png", "BlackPawnC7.png", "BlackPawnD7.png", "BlackPawnE7.png", "BlackPawnF7.png", "BlackPawnG7.png", "BlackPawnH7.png"]
 	
-	#On renomme toutes les images avec l'orientation en debut
-	listeImagesAFusionner = []
-	listeImagesAFusionnerTriee = []
-	nombreImages = len(listeImages)
-	for i in range(4):
-		orientation = listeOrientations[i]
-		for j in range(nombreImages):
-			listeImagesAFusionner += [orientation + listeImages[j]] #produit le nom del'image sous la forme "frontWhiteRookA1" et l'insere dans la liste
-		listeImagesAFusionnerTriee += trieListeImageAFusionnerSelonOrientation(listeImagesAFusionner[nombreImages*i : (nombreImages*(i+1))+1], orientation)
-		imageFusionnee = superposePlusieursImages(listeImagesAFusionnerTriee[nombreImages*i : (nombreImages*(i+1))+1], orientation)
-		plt.imsave(orientation+nomImage+".png",imageFusionnee)
-
-	return (listeImagesAFusionner)
-	#End
+    #On renomme toutes les images avec l'orientation en debut
+    listeImagesAFusionner = []
+    listeImagesAFusionnerTriee = []
+    nombreImages = len(listeImages)
+    for i in range(4):
+	orientation = listeOrientations[i]
+	for j in range(nombreImages):
+            listeImagesAFusionner += [orientation + listeImages[j]] #produit le nom del'image sous la forme "frontWhiteRookA1" et l'insere dans la liste
+	    listeImagesAFusionnerTriee += trieListeImageAFusionnerSelonOrientation(listeImagesAFusionner[nombreImages*i : (nombreImages*(i+1))], orientation)
+	    imageFusionnee = superposePlusieursImages(listeImagesAFusionnerTriee[nombreImages*i : (nombreImages*(i+1))], orientation)
+	    plt.imsave(orientation+nomImage+".png",imageFusionnee)
+    return (listeImagesAFusionner)
+    #End
 
 	
 
 #%%____________________________________________________________________________
 # Fonction qui renvoie les dimensions d'une image (7lignes)
 def dimensionImage(image) :
-	"""Fonction qui renvoie les dimensions d'une image 
-			paramètre : 1 np.array (au moins bidimensionnel) = une image
-			retourne : 1 2-uple = les dimensions de l'image
-	"""
-	#Begin
-	colonnes = len(image[0])
-	lignes = len(image)
-
-	return (lignes, colonnes)
-	#End
+    """Fonction qui renvoie les dimensions d'une image 
+		paramètre : 1 np.array (au moins bidimensionnel) = une image
+		retourne : 1 2-uple = les dimensions de l'image
+    """
+    #Begin
+    colonnes = len(image[0])
+    lignes = len(image)
+    return (lignes, colonnes)
+    #End
 
 	
 	
 #%%____________________________________________________________________________
 # Fonction qui cree l'image à projeter	(46lignes)
-def creeImageHolographique(nomImage):
-	"""Fonction qui regroupe les 4 images correspondant aux 4 orientations en une seule image a projeter sur la pyramide
-			paramètre : 1 String  = le nom de l'image a fusionner (sans l'orientation devant)
-			retourne : 1 np.array = l'image fusionnee
-	"""
-	#Begin
+def regroupe4ImagesEn1(nomImage):
+    """Fonction qui regroupe les 4 images correspondant aux 4 orientations en une seule image a projeter sur la pyramide
+		paramètre : 1 String  = le nom de l'image a fusionner (sans l'orientation devant)
+		retourne : 1 np.array = l'image fusionnee
+    """
+    #Begin
 
-	#importation des images
-	front = mpimg.imread("front"+nomImage+".png")
-	right = mpimg.imread("right"+nomImage+".png")
-	back = mpimg.imread("back"+nomImage+".png")	
-	left = mpimg.imread("left"+nomImage+".png")
-	print("Images importées")
+    #importation des images
+    front = mpimg.imread("front"+nomImage+".png")
+    right = mpimg.imread("right"+nomImage+".png")
+    back = mpimg.imread("back"+nomImage+".png")	
+    left = mpimg.imread("left"+nomImage+".png")
+    print("Images importées")
 
-	#rotation des images
-	rotateRight = ndimage.rotate(right, 270)
-	rotateBack = ndimage.rotate(back, 180)
-	rotateLeft = ndimage.rotate(left, 90)
-	print("Images retournées")
+    #rotation des images
+    rotateRight = ndimage.rotate(right, 270)
+    rotateBack = ndimage.rotate(back, 180)
+    rotateLeft = ndimage.rotate(left, 90)
+    print("Images retournées")
 
-	#creation de la "base" ou "coller" les quatre images ci-dessus
-	lignes, colonnes = dimensionImage(front) 
-	print("les dimensions sont : lignes  = ",lignes," colonnes = ", colonnes)
-	print("on entre dans le if ? : ", ((lignes, colonnes) == dimensionImage(back))," and ",( (colonnes, lignes) == dimensionImage(rotateRight) == dimensionImage(rotateLeft)) )
+    #creation de la "base" ou "coller" les quatre images ci-dessus
+    lignes, colonnes = dimensionImage(front) 
+    print("les dimensions sont : lignes  = ",lignes," colonnes = ", colonnes)
+    print("on entre dans le if ? : ", ((lignes, colonnes) == dimensionImage(back))," and ",( (colonnes, lignes) == dimensionImage(rotateRight) == dimensionImage(rotateLeft)) )
 	
-	if ( ((lignes, colonnes) == dimensionImage(back)) and ( (colonnes, lignes) == dimensionImage(rotateRight) == dimensionImage(rotateLeft)) ) :
-		print("les dimensions sont bonnes")
-		dimension = lignes*2 + colonnes
-		base = np.zeros((dimension,dimension,3), dtype='f')
-		print("base créée : dimension = ", dimension," et ", dimensionImage(left) )
+    if ( ((lignes, colonnes) == dimensionImage(back)) and ( (colonnes, lignes) == dimensionImage(rotateRight) == dimensionImage(rotateLeft)) ) :
+    	print("les dimensions sont bonnes")
+	dimension = lignes*2 + colonnes
+	base = np.zeros((dimension,dimension,3), dtype='f')
+	print("base créée : dimension = ", dimension," et ", dimensionImage(left) )
 
 	#insertion des images dans la base
-		for i in range(dimension):
-			for j in range(dimension):			
-				#insertion de back
-				if ( (i < lignes) and (lignes <= j < lignes + colonnes) ):
-					base[i][j] = rotateBack[i][j - lignes][0:3]
-					#print("pixel position : i = ",i," et j = ",j)
-				#insertion de right 
-				elif ( (lignes <= i < lignes + colonnes) and (j < lignes) ):
-					base[i][j] = rotateRight[i - lignes][j][0:3]
-					#print("pixel position : i = ",i," et j = ",j)
-				#insertion de left
-				elif ( (lignes <= i < lignes + colonnes) and (lignes + colonnes <= j < dimension ) ) :
-					base[i][j] = rotateLeft[i - lignes][j - lignes - colonnes][0:3]
-					#print("pixel position : i = ",i," et j = ",j)
-				#insertion de front
-				elif ( (lignes + colonnes <= i < dimension) and (lignes <= j < lignes + colonnes) ) :
-					base[i][j] = front[i - lignes - colonnes][j - lignes][0:3]
-					#print("pixel position : i = ",i," et j = ",j)
+	for i in range(dimension):
+	    for j in range(dimension):			
+	    #insertion de back
+		if ( (i < lignes) and (lignes <= j < lignes + colonnes) ):
+		    base[i][j] = rotateBack[i][j - lignes][0:3]
+		    #print("pixel position : i = ",i," et j = ",j)
+	    #insertion de right 
+		elif ( (lignes <= i < lignes + colonnes) and (j < lignes) ):
+		    base[i][j] = rotateRight[i - lignes][j][0:3]
+		    #print("pixel position : i = ",i," et j = ",j)
+	    #insertion de left
+		elif ( (lignes <= i < lignes + colonnes) and (lignes + colonnes <= j < dimension ) ) :
+		    base[i][j] = rotateLeft[i - lignes][j - lignes - colonnes][0:3]
+		    #print("pixel position : i = ",i," et j = ",j)
+	    #insertion de front
+		elif ( (lignes + colonnes <= i < dimension) and (lignes <= j < lignes + colonnes) ) :
+		    base[i][j] = front[i - lignes - colonnes][j - lignes][0:3]
+		    #print("pixel position : i = ",i," et j = ",j)
 
-		return(base)		
-
-	return("erreur : les images doivent avoir les memes dimentions")	
-	#End	
+	return(base)		
+    #si les dimensions ne sont pas bonnes
+    return("erreur : les images doivent avoir les memes dimentions")	
+    #End	
 
 	
 	
 #%%____________________________________________________________________________
 # Fonction qui cree l'image à projeter (XXXlignes)
 def creePlateauHolographique(nomImage):
-	"""Fonction qui regroupe les 4 images correspondant aux 4 orientations en une seule image a projeter sur la pyramide
-			paramètre : 1 string = le nom de l'image a fusionner (sans l'orientation devant)
-			retourne : 	1 liste = liste des images à fusionner (triee)
-						1 np.array = l'image fusionnee
-	"""
-	#Begin
-	listeImagesAFusionner = imagesPlateauInitial(nomImage)
-	imageHolographique = creeImageHolographique(nomImage)
-
-	return (listeImagesAFusionner, imageHolographique)
-	#End
+    """Fonction qui regroupe les 4 images correspondant aux 4 orientations en une seule image a projeter sur la pyramide
+		paramètre : 1 string = le nom de l'image a fusionner (sans l'orientation devant)
+		retourne : 	1 liste = liste des images à fusionner (triee)
+				1 np.array = l'image fusionnee
+    """
+    #Begin
+    listeImagesAFusionner = imagesPlateauInitial(nomImage)	#cree les quatres images du plateau initial et la nomme : orientation+nomImage+.png
+    imageHolographique = regroupe4ImagesEn1(nomImage)		#regroupe les quatres images du nom "orientation+nomImage+.png" en une seule à projeter
+    return (listeImagesAFusionner, imageHolographique)
+    #End
 
 
 	
